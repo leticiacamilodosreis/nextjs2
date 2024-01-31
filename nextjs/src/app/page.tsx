048a969cee0f1ad5c5fb5a96d08958a23258ea8b
Desktop-motiva <lelecamilobru@gmail.com>
2024-01-31 14:33:47 -0300

    update

---
+++ nextjs/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <body>
+        <div>
+        <h1>ola mundo</h1>
+
+        <p>olha so que site divertido</p>
+        
+      </div>
+
+
+      </body>
+    </main>
+
+  );
+}
